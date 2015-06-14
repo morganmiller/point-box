a1ba9ac344666f5e01f442dddddc727d011d1086
Morgan Miller <m.miller722@gmail.com>
2015-06-13 20:48:38 -0600

    add user creation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash.now[:errors] = @user.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+private
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
