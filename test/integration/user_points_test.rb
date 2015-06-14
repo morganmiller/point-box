83ef9c5e641a2cdbb9e53e059718b7f3776c666d
Morgan Miller <m.miller722@gmail.com>
2015-06-13 22:41:44 -0600

    hold user redemption test

--- test/integration/user_points_test.rb
+++ test/integration/user_points_test.rb
@@ -10,4 +10,15 @@ class UserPointsTest < ActionDispatch::IntegrationTest
     visit user_path(user)
     assert page.has_content?("You currently have 15 points.")
   end
+  # 
+  # test "user can redeem points" do
+  #   user = User.create(username: "tigerlily",
+  #                      password: "sunshine",
+  #                      points: 50)
+  # 
+  #   ApplicationController.any_instance.stubs(:current_user).returns(user)
+  #   visit user_path(user)
+  #   click_link "Buy some things?"
+  #   
+  # end
 end
