1f8b1a66a222e4bee54179f39473a27cb6515420
Morgan Miller <m.miller722@gmail.com>
2015-06-14 19:47:36 -0600

    remove fixture comment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,6 @@ require 'mocha/mini_test'
 require 'pry'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-
   # Add more helper methods to be used by all tests here...
 end
 
